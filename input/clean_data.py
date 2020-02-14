c374e916665f70fe4335a7dc1963b3d9ae88b895
HamzaKV <hamzakv333@gmail.com>
2020-02-13 19:06:53 -0500

    Data Cleanup

--- input/clean_data.py
+++ input/clean_data.py
@@ -1,5 +1,8 @@
-with open('weather_data_miami_hollywood.csv', 'r') as input_file, open('weather_data_miami_hollywood_temp.csv', 'w+') as output_file:
-    for line in input_file:
-        if not(line == "\n"):
-            output_file.write(line)
+files = ['BelleGlade', 'BonitaSprings', 'CapeCoral', 'Goodland', 'HomesteadAirReserveBase', 'Immokalee', 'KingsPoint', 'LehighAcres', 'LibertyPoint', 'Turkeyfoot', 'WestPalmBeach']
+for city in files:
+    with open(city + '.csv', 'r') as input_file, open('weather_data_' + city +'.csv', 'w+') as output_file:
+        output_file.write('time,summary,precipIntensity,precipProbability,temperature,apparentTemperature,dewPoint,humidity,pressure,windSpeed,windBearing,cloudCover,uvIndex,visibility,nearestStormDistance')
+        for line in input_file:
+            if not(line == "\n"):
+                output_file.write(line)
         
